91ca1cf1c591f96834481d4f97d3dc97ee5f7f83
CopyG <benjaminmontecinosm2017@hotmail.com>
2026-06-08 23:09:21 -0400

    avanze 1 tarea 4

---
+++ 2026_1/tarea_4/tarea4.py
@@ -0,0 +1,39 @@
+#pregunta 1
+def lista_pedidos(nombre_pedidos):
+    archivo_pedidos = open(nombre_pedidos, 'r')
+    #    id_pedido, cliente, tiempo_estimado, tiempo_real, costo, reclamo, id_producto
+    #e.g: 1      , Juan      ,  30           ,   45      , 8000 ,    1   ,  101
+
+    costo = []
+    contador = 0
+    for linea in archivo_pedidos:
+        if contador != 0:
+            temp = str(linea.strip())
+            temp = temp.split(";")
+            temp = [int(temp[0]),int(temp[-3])]
+           # print(temp) #debug, delete later :v
+            
+            costo.append(temp)
+        contador+=1
+    
+    
+
+    print(costo)
+    
+    archivo_pedidos.close()
+    return costo
+    
+#pregunta 2
+def desempeño(nombre_pedidos, nombre_productos):
+    
+    tipos = {"Problemático":[],"Eficiente":[],"Costoso":[]}
+    archivo_pedidos = open(nombre_pedidos, 'r')
+    archivos_produc = open(nombre_productos, 'r')
+    #    id_producto, nombre        , categoria, precio_base
+    #e.g:   101     ,Hamburguesa    ,Comida    ,5000 
+    
+    
+    
+    
+    archivo_pedidos.close()
+    archivos_produc.close()
