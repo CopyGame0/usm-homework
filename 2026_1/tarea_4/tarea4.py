636fca9f4f13e82035387e5e22128d7a8499fbec
lds <benjaminmontecinosm2017@hotmail.com>
2026-06-09 14:06:46 -0400

    Avanze 2 tarea 4

--- 2026_1/tarea_4/tarea4.py
+++ 2026_1/tarea_4/tarea4.py
@@ -10,7 +10,7 @@ def lista_pedidos(nombre_pedidos):
         if contador != 0:
             temp = str(linea.strip())
             temp = temp.split(";")
-            temp = [int(temp[0]),int(temp[-3])]
+            temp = [temp[0],int(temp[-3])]
            # print(temp) #debug, delete later :v
             
             costo.append(temp)
@@ -24,12 +24,32 @@ def lista_pedidos(nombre_pedidos):
 def desempeño(nombre_pedidos, nombre_productos):
     
     tipos = {"Problemático":[],"Eficiente":[],"Costoso":[]}
+    """e.g.
+    {'Problemático': [[8000, 'Hamburguesa', 30, 45, 'Comida'], [12000, 'Sushi',40, 55, 'Comida']],
+     'Costoso': [[8000, 'Hamburguesa', 30, 45, 'Comida'],[12000, 'Sushi', 40, 55, 'Comida']],
+     'Eficiente': [[5000, 'Hamburguesa', 20,18, 'Comida'], [6000, 'Pizza', 25, 20, 'Comida']]}
+    """
     
     archivos_produc = open(nombre_productos, 'r')
     #    id_producto, nombre        , categoria, precio_base
     #e.g:   101     ,Hamburguesa    ,Comida    ,5000 
-    
-    costos = lista_pedidos(nombre_pedidos) 
-    
-    
+
+    productos= {}
+    for linea in archivos_produc:
+        
+        temp = linea.split(";")
+        productos[temp[0]] = temp[1:]
+        #{id_producto: [nombre, categoria, precio_base]} tendremos un indice extra, pero es un diccionario asi que no importa :P
     archivos_produc.close()
+
+    costos_pedidos = lista_pedidos(nombre_pedidos) 
+    
+    archivo_pedidos = open(nombre_pedidos, 'r')
+
+    id_pedidos = {}
+    for linea in archivo_pedidos:
+        temp = linea.split(";")
+        id_pedidos[temp[0]] = temp[1:]
+        
+
+    archivo_pedidos.close() 
